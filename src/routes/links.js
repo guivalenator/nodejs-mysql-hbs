1e89a7e14130fdacefe58e0ad41c2d0a29691299
HOSPITAL - Sistemas01 <sistemas01@hospital.local>
2021-10-19 17:03:10 -0500

    v0

--- src/routes/links.js
+++ src/routes/links.js
@@ -1,6 +1,31 @@
 const express = require('express');
+const { body } = require('express-validator');
 const router = express.Router();
 
 const pool = require('../database')
 
-module.exports = router;
+router.get('/add/', (req, res) => {
+    res.render('links/add.hbs');
+});
+
+router.post('/add/', async   (req, res) => {
+    const{title, url, description}= req.body;
+    const newLink= {
+        title,
+        url,
+        description
+    };
+    await pool.query('INSERT INTO links SET?' [newLink])
+    console.log(newLink);
+    res.redirect('/links');
+});
+
+router.get('/', async (req, res) =>{
+    const links = await pool.query('SELECT * FROM links');
+    console.log(links);
+    res.send('links/list',{links });
+}
+)
+
+
+module.exports = router; 
